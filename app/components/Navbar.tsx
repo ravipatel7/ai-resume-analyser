90036952cf04268ffeedf7827c941ba1e82a151c
Ravi Patel <raviprksh77@gmail.com>
2025-08-10 18:40:37 +0530

    Authentication

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+        <Link to="/"><p className="text-2xl font-bold capitalize text-gradient">ResumeAI</p></Link>
+        <Link to="/upload" className="primary-button w-fit">Upload Resume</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
